99b7670b972ee46743ff70a479ffe75766b032e2
มาซาดะโคลแมน <masada.hir@turbo.co.th>
2024-09-11 17:12:02 +0700

    improve-sub-set

--- testcases/testScripts/logic/้http/http_assertion.logic.ts
+++ testcases/testScripts/logic/้http/http_assertion.logic.ts
@@ -2,23 +2,24 @@ import { expect } from "chai"
 import IndianReportLogic from "../report.logic"
 import Formatter from "../../class/formatter.class"
 import AssertionLogic from "../assertion.logic"
+import { HttpRequest, HttpResponse } from "../../interface/file_interface/http_file.model"
 
 class HttpAssertionLogic {
 
-    static SubSet(actualRes: Record<string, any>, expectRes: Record<string, any>): void {
+    static SubSet(actualRes: HttpResponse, expectRes: HttpResponse): void {
         if (!expectRes) return
-        const expectedBody = Formatter.Exec(expectRes["body"])
+        const expectedBody = Formatter.Exec(expectRes.body)
         IndianReportLogic.AddTestStep({
             actualResult: {
-                status: actualRes["status"],
-                body: actualRes["body"]
+                status: actualRes.status,
+                body: actualRes.body
             }, expectedResult: {
-                status: expectRes["status"],
+                status: expectRes.status,
                 body: expectedBody
             }
         })
-        if ("status" in expectRes) expect(actualRes["status"]).equal(expectRes["status"])
-        if ("body" in expectRes) AssertionLogic.SubSet(actualRes["body"], expectedBody, "")
+        if ("status" in expectRes) expect(actualRes.status).equal(expectRes.status)
+        if ("body" in expectRes) AssertionLogic.SubSet(actualRes.body, expectedBody, "")
     }
 }
 
