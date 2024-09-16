a793f11d0c7b7db444438c1f6bcc4830a31fce7d
มาซาดะโคลแมน <masada.hir@turbo.co.th>
2024-09-16 15:21:51 +0700

    Pg Exec {string}

--- testcases/testScripts/controller/postgresql/postgresql.cucumber.ts
+++ testcases/testScripts/controller/postgresql/postgresql.cucumber.ts
@@ -10,45 +10,50 @@ import File from '../../util/file.util';
 export class PostgresqlController {
   @when('StepDef.{string} fileName.{string} Postgres query', { timeout: Cfg.stepTimeOut })
   public async PgQueryJsonFileWithComment(_des: string, file: string): Promise<void> {
-    await PgLogic.Exec(file)
+    await PgLogic.Exec(file);
   }
   @when('{string} Postgres query', { timeout: Cfg.stepTimeOut })
   public async PgQueryJsonFile(file: string): Promise<void> {
-    await PgLogic.Exec(file)
+    await PgLogic.Exec(file);
   }
   @given('Truncate Table {string}.{string}', { timeout: Cfg.stepTimeOut })
   public async TruncateTable(schema: string, table: string): Promise<void> {
-    await PgProtocol.Query(`TRUNCATE TABLE ${schema}.${table}  RESTART IDENTITY CASCADE ;`)
+    await PgProtocol.Query(`TRUNCATE TABLE ${schema}.${table}  RESTART IDENTITY CASCADE ;`);
   }
   @given('Truncate Tables', { timeout: Cfg.stepTimeOut })
   public async TruncateTables(bddTable: DataTable): Promise<void> {
-    await PgProtocol.Query(Formatter.Exec(bddTable.hashes())
-      .reduce((acc: string, row: Record<string, any>) => `${acc} 
-        TRUNCATE TABLE ${row["schemaName"]}.${row["tableName"]} 
-        RESTART IDENTITY CASCADE ;`, ""))
+    await PgProtocol.Query(
+      Formatter.Exec(bddTable.hashes()).reduce(
+        (acc: string, row: Record<string, any>) => `${acc} 
+        TRUNCATE TABLE ${row['schemaName']}.${row['tableName']} 
+        RESTART IDENTITY CASCADE ;`,
+        ''
+      )
+    );
   }
   @given('Insert data {string}.{string}', { timeout: Cfg.stepTimeOut })
   public async InsertData(schema: string, table: string, bddTable: DataTable): Promise<void> {
-    const tablePath: string = ` ${schema}.${table}`
+    const tablePath: string = ` ${schema}.${table}`;
     const sqlValues: string = Formatter.Exec(bddTable['rawTable'].slice(1))
-      .map((row: string[]) => PgLogic.FormatSqlVal(row)).join(',');
+      .map((row: string[]) => PgLogic.FormatSqlVal(row))
+      .join(',');
     await PgProtocol.Query(`INSERT INTO ${tablePath} (${bddTable['rawTable'][0].join(',')}) VALUES ${sqlValues};
         SELECT setval(pg_get_serial_sequence('${tablePath}', 'id'), coalesce(MAX(id), 1))
-        from ${tablePath};`)
-    PgLogic.addTableList(tablePath)
+        from ${tablePath};`);
+    PgLogic.addTableList(tablePath);
   }
   @given('Truncate inserted table', { timeout: Cfg.stepTimeOut })
   public async TruncateInsertedTable(): Promise<void> {
-    await PgProtocol.Query(PgLogic.tableList.map(table =>
-      `TRUNCATE TABLE ${table} RESTART IDENTITY CASCADE ;`
-    ).join("\n"))
-    PgLogic.tableList = []
+    await PgProtocol.Query(
+      PgLogic.tableList.map((table) => `TRUNCATE TABLE ${table} RESTART IDENTITY CASCADE ;`).join('\n')
+    );
+    PgLogic.tableList = [];
   }
   @given('Insert from sql: {string}', { timeout: Cfg.stepTimeOut })
   @given('Exec sql: {string}', { timeout: Cfg.stepTimeOut })
   @given('Pg Exec {string}', { timeout: Cfg.stepTimeOut })
   public async InsertFromSql(file: string): Promise<void> {
-    const fileText: string = await File.readText(`sql/${file}`)
-    await PgProtocol.Query(fileText)
+    const fileText: string = await File.readText(`sql/${file}`);
+    await PgProtocol.Query(fileText);
   }
 }
