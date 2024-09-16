6d8f9ad94dccedbc04eb81beca35124e770de927
มาซาดะโคลแมน <masada.hir@turbo.co.th>
2024-09-16 16:22:28 +0700

    StorageCollectorController

--- testcases/testScripts/controller/storage/storage_collector.controller.ts
+++ testcases/testScripts/controller/storage/storage_collector.controller.ts
@@ -2,17 +2,26 @@ import { binding, given } from 'cucumber-tsflow';
 import Cfg from '../../class/config.class';
 import CollectorLogic from '../../logic/collector.logic';
 import { ContentTypeEnum } from '../../enum/content.enum';
+import ResClass from '../../class/response.class';
 
 @binding()
 export class StorageCollectorController {
-  @given('Collect Response parameter data fileName.{string}', { timeout: Cfg.stepTimeOut })
-  @given('Collect Response param.{string}', { timeout: Cfg.stepTimeOut })
-  public async CollectResponseJsonFile(file: string): Promise<void> {
-    CollectorLogic.JsonFile(file, ContentTypeEnum.http);
-  }
-  @given('Collect Query parameter data fileName.{string}', { timeout: Cfg.stepTimeOut })
-  @given('Collect Query param.{string}', { timeout: Cfg.stepTimeOut })
-  public async CollectQueryResultJsonFile(file: string): Promise<void> {
-    CollectorLogic.JsonFile(file, ContentTypeEnum.postgresql);
-  }
+    @given('Collect Response parameter data fileName.{string}', { timeout: Cfg.stepTimeOut })
+    @given('Collect Response param.{string}', { timeout: Cfg.stepTimeOut })
+    public async CollectResponseJsonFile(file: string): Promise<void> {
+        await CollectorLogic.JsonFile(file, ContentTypeEnum.http);
+    }
+    @given('Collect Query parameter data fileName.{string}', { timeout: Cfg.stepTimeOut })
+    @given('Collect Query param.{string}', { timeout: Cfg.stepTimeOut })
+    public async CollectQueryResultJsonFile(file: string): Promise<void> {
+        await CollectorLogic.JsonFile(file, ContentTypeEnum.postgresql);
+    }
+    @given('Collect Query Path:{string} Var:{string}', { timeout: Cfg.stepTimeOut })
+    public CollectQueryByPath(path: string, varName: string): void {
+        CollectorLogic.CollectByPath(ResClass.Query, path, varName)
+    }
+    @given('Collect Response Path:{string} Var:{string}', { timeout: Cfg.stepTimeOut })
+    public CollectResponseByPath(path: string, varName: string): void {
+        CollectorLogic.CollectByPath(ResClass.Http.body, path, varName)
+    }
 }
