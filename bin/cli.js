258464a1da0050a852f87f3315659c3acb4a6504
มาซาดะโคลแมน <masada.hir@turbo.co.th>
2024-10-25 09:39:12 +0700

    realtime-cucumber-log

--- bin/cli.js
+++ bin/cli.js
@@ -1,18 +1,40 @@
 #!/usr/bin/env node
-const { exec } = require("child_process");
 
-async function runCommand(command) {
-    console.warn(`Running : ${command}`);
-    return await new Promise((resolve, reject) => {
-        exec(command, { cwd: __dirname, shell: true, stdio: 'inherit' }, (error) => {
-            if (error) {
-                reject(`Error: ${error.message}`);
+const { spawn } = require("child_process");
+
+function runCommand(command) {
+    // console.warn(`Running : ${command}`)
+    return new Promise((resolve,reject) => {
+        const child = spawn(command, {
+            stdio: 'inherit',cwd: __dirname, 
+            shell: true
+        });
+        child.on('error', (error) => {
+            console.error(`error: ${error.message}`);
+        });
+
+        child.on('exit', (code) => {
+            if (code === 0) {
+                resolve();
+            } if (code !== 0) {
+                reject(`exited code: ${code}`);
             }
-            resolve();
+        });
+
+        child.on('close', (code) => {
+            if (code === 0) {
+                resolve();
+            } else {
+                reject(`exited code: ${code}`);
+            }
         });
     });
 }
 
 runCommand("npm i").then(() => {
     require("../index.js");
+}).catch((err)=>{
+    console.error(`Building Error : ${err}`)
 })
+
+
